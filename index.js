80799c31cc9f7623c8bdc26d3ff2269b38a2c311
E Abella <eabella@Es-MacBook-Air.local>
2024-06-24 14:03:40 -0700

    updated product route js file

--- index.js
+++ index.js
@@ -7,15 +7,16 @@ const app = express();
 
 // middleware
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-
+// ROUTES for products
+//app.use("api/products", productRoute);
 
 // reponse screen
 // client = request, response from the server = response
 app.get('/', (req, res) => {
-    res.send("Hello World Update");
+    res.send('Hello World Update');
 });
-
 
 app.get('/api/products', async (req, res) => {
     try {
@@ -26,6 +27,7 @@ app.get('/api/products', async (req, res) => {
     }
 });
 
+// GET a product 
 app.get('/api/product/:id', async (req, res) => {  
     try {
         const { id } = req.params;
@@ -48,13 +50,13 @@ app.post('/api/products', async (req, res) => {
 });
 
 // UPDATE a product 
-app.put('/api/product/:id', async (req, res) => { 
+app.put('/api/products/:id', async (req, res) => { 
     try { 
         const { id } = req.params;
         const product = await Product.findByIdAndUpdate(id, req.body); 
         
         if(!product) {
-            return res.status(404).json({message: "Product not found"});
+            return res.status(404).json({message: 'Product not found'});
         }
 
         // res.status(200).json(product);
@@ -66,16 +68,35 @@ app.put('/api/product/:id', async (req, res) => {
     }
 });
 
+// DELETE a product 
+app.delete('/api/products/:id', async (req, res) => { 
+    try { 
+        const { id } = req.params;
+        const product = await Product.findByIdAndDelete(id); 
+        
+        if(!product) {
+            return res.status(404).json({message: 'Product not found'});
+        }
+
+        // res.status(200).json(product);
+        // const updatedProduct = await Product.findById(id); 
+        res.status(200).json({message: 'Product deleted successfully'});
+
+    } catch (err){
+        res.status(500).json({message: err.message}); // display the response error message to the server 
+    }
+});
+
 
 // connection string from MondoDB
 // collection name is 'Node-API'
-mongoose.connect("mongodb+srv://ebelbell:MangoMango3!!!@sba319.ankjbb2.mongodb.net/Node-API?retryWrites=true&w=majority&appName=SBA319")
+mongoose.connect('mongodb+srv://ebelbell:MangoMango3!!!@sba319.ankjbb2.mongodb.net/Node-API?retryWrites=true&w=majority&appName=SBA319')
 .then(() => {
-    console.log("Database is connected!");
+    console.log('Database is connected!');
     app.listen(4000, () => {
         console.log('Server is running on port 4000');
     });
 })
 .catch(() => {
-    console.log("Database failed.");
+    console.log('Database failed.');
 });
