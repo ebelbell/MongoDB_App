9ecb738852e74bf048ad18d1c33a9f9da85cf6be
E Abella <eabella@Es-MacBook-Air.local>
2024-06-24 00:24:56 -0700

    added dev and nodemon to the dependencies so that the server is automatically updated

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 
-// app.get('/', function (req,res) {
-//     res.send("Hello World")
-// });
-
+// console screen
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
 })
+
+// reponse screen
+// client = request, response from the server = response
+app.get('/', (req, res) => {
+    res.send("Hello World Update");
+});
