ef2d112995d4f3e9810761526f3e3b5540344edf
E Abella <eabella@Es-MacBook-Air.local>
2024-06-24 08:03:17 -0700

    added onto the products.model.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const app = express();
 // using Node.js
 const mongoose = require('mongoose');
-// using ES6 imports
-//import mongoose from 'mongoose'; 
+// import the product model 
+const Product = require('./models/product.model')
+const app = express();
 
-// console screen
-app.listen(4000, () => {
-    console.log('Server is running on port 4000');
-});
+// middleware
+app.use(express.json());
+
+
 
 // reponse screen
 // client = request, response from the server = response
@@ -16,11 +16,38 @@ app.get('/', (req, res) => {
     res.send("Hello World Update");
 });
 
+
+app.get('/api/products', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200). json(products);
+    } catch (err)  {
+        res.status(500).json({message: err.message});
+    }
+});
+
+app.get('/api/products/:id', async (req, res) => {  }
+
+
+app.post('/api/products', async (req, res) => {
+    // console.log(req.body);
+    // res.send(req.body); // "Data received"
+    try { // save the Product by using await to 
+        const product = await Product.create(req.body);
+        res.status(200).json(product)
+    } catch (err){
+        res.status(500).json({message: err.message}); // display the response error message to the server 
+    }
+});
+
 // connection string from MondoDB
 // collection name is 'Node-API'
 mongoose.connect("mongodb+srv://ebelbell:MangoMango3!!!@sba319.ankjbb2.mongodb.net/Node-API?retryWrites=true&w=majority&appName=SBA319")
 .then(() => {
     console.log("Database is connected!");
+    app.listen(4000, () => {
+        console.log('Server is running on port 4000');
+    });
 })
 .catch(() => {
     console.log("Database failed.");
